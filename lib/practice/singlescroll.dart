f72feff86f3d8c8c13214657e1331e9cf6655e87
YuKyung0622 <kkomang0622@gmail.com>
2024-05-20 01:04:24 +0900

    flutter

---
+++ lib/practice/singlescroll.dart
@@ -0,0 +1,102 @@
+import 'package:contact/main.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp() as Widget);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('test title'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height:MediaQuery.of(context).size.height/2,
+              child:SingleChildScrollView(
+                child:Column(
+                  children:[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      color: Colors.greenAccent,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      color: Colors.cyanAccent,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height:MediaQuery.of(context).size.height/2,
+              child:SingleChildScrollView(
+                child:Column(
+                  children:[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      color: Colors.yellowAccent,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      color: Colors.pinkAccent,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height:MediaQuery.of(context).size.height/2,
+              child:SingleChildScrollView(
+                child:Column(
+                  children:[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
