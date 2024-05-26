4054e56adb1a35614f23a4f354025391ca0c7a37
YuKyung0622 <kkomang0622@gmail.com>
2024-05-27 00:54:46 +0900

    flutter

---
+++ lib/practice/product.dart
@@ -0,0 +1,20 @@
+class Product {
+  final int id;
+  final String title;
+  final String desc;
+
+  Product({required this.id, required this.title, required this.desc});
+
+  factory Product.fromJson(Map<Stirng, dynamic> json) {
+    return Product(
+        id: json['id'],
+        title: json['title'],
+        desc: json['desc']
+    );
+  }
+  Map<String, dynamic> toJson() => {
+          'id' : id,
+          'title' : title,
+          'desc' : desc,
+  };
+}
---
+++ lib/practice/refreshindicator.dart
@@ -0,0 +1,51 @@
+import 'package:contact/main.dart';
+import 'package:contact/practice/product.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp() as Widget);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<Product>> productList;
+  Dio dio = Dio();
+
+  @override
+  void initState() {
+    super.initState();
+    productList = getProductData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter App'),
+      ),
+      body: Container(
+      ),
+    );
+  }
+}
