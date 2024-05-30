03193c49a7c4753a7f7b8c161443c80782c654d1
YuKyung0622 <kkomang0622@gmail.com>
2024-05-31 01:14:57 +0900

    first commit

--- lib/practice/locator/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:contact/practice/service/album_service.dart';
-import 'package:get_it/get_it.dart';
-
-GetIt locator = GetIt.instance;
-
-initLocator() {
-  locator.registerLazySingleton<AlbumService>(()=> AlbumSerivceImplementation());
-}
