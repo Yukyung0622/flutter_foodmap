1318f2eecb6e17e3b9dd1a67cec8b8f3472e65c5
YuKyung0622 <kkomang0622@gmail.com>
2024-06-09 01:54:04 +0900

    flutter

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget{
-  const MyButton ({super.key});
+  final Function()? onTap;
+
+  const MyButton({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      margin: const EdgeInsets.symmetric(horizontal: 25),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(8),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
       ),
       child: const Center(
         child: Text(
@@ -21,6 +25,7 @@ class MyButton extends StatelessWidget{
             fontSize: 16,
           ),
         ),
+       ),
       ),
     );
   }
