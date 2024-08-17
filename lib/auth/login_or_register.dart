37b890df0183a51f83f5b770b662846e37267be5
YuKyung0622 <kkomang0622@gmail.com>
2024-08-18 02:44:36 +0900

    flutter

---
+++ lib/auth/login_or_register.dart
@@ -0,0 +1,32 @@
+import 'package:contact/login/loginpage.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginOrRegister extends StatelessWidget {
+  const LoginOrRegister({super.key});
+
+
+  @override
+  State<LoginOrRegister> creatsState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegister extends State<LoginOrRegister> {
+
+  //initially, show login page
+  bool showLoignPage = true;
+
+  //toggle between login and register page
+  void togglePages(){
+    setState(() {
+      showLoginPage = !showLoignPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(showLoginPage) {
+      return LoginPage(onTap: togglePages);
+    } else{
+      return RegisterPage(onTap: togglePages);
+    }
+  }
+}
