fef810f6fae2f0d1ff2e867e3835e52e78a5884b
YuKyung0622 <kkomang0622@gmail.com>
2024-08-22 02:55:14 +0900

    flutter

--- lib/auth/login_or_register.dart
+++ lib/auth/login_or_register.dart
@@ -1,7 +1,6 @@
 import 'package:contact/login/loginpage.dart';
 import 'package:flutter/material.dart';
-
-import '../login/register_page.dart';
+import 'package:contact/login/register_page.dart';
 
 class LoginOrRegister extends StatelessWidget {
   const LoginOrRegister({super.key});
